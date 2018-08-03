97a1efb340ffb7f8ba53696be5fe07e94d626b49
PorPit <692066768@qq.com>
2018-08-03 13:04:36 +0800

    fixVersion

--- src/main/java/com/porpit/crafttweakersync/CraftTweakerSync.java
+++ src/main/java/com/porpit/crafttweakersync/CraftTweakerSync.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class CraftTweakerSync {
     public static final String MODID = "crafttweakersync";
     public static final String NAME = "CraftTweakerSync";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @SidedProxy(clientSide = "com.porpit.crafttweakersync.client.ClientProxy", serverSide = "com.porpit.crafttweakersync.common.CommonProxy")
     public static CommonProxy proxy;
